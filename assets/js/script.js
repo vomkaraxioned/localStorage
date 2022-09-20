ccd4968d6b69776d1eda1dd9cc40b47656e03562
vomkaraxioned <omkarv@axioned.com>
2022-09-20 16:49:29 +0530

    js started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,11 @@
 /* Author: 
 
 */
+const form = document.querySelector("form[name=login");
+form.addEventListener("submit", () => {
+    let username = document.forms["login"]["uname"].value;
+    let password = document.forms["login"]["pass"].value;
+    if (username == "" && password == "") {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+});
